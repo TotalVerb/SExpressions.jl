fea840b1dbc750765c447fa2f078a5cf512fb35a
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-06-10 21:50:22 -0400

    concept of a functional lisp

---
+++ src/MiniLisp.jl
@@ -0,0 +1,33 @@
+# A mini purely functional lisp with continuations
+# warning: no garbage collection yet
+
+# nowhere near complete; enable when done
+
+#=
+module MiniLisp
+
+using ..Lists
+
+abstract Continuation
+
+immutable
+
+const BUILTINS = List(
+    List(:(+), +),
+    List(:(-), -),
+    List(:cons, Cons),
+    List(:car, car),
+    List(:cdr, cdr))
+
+function compute(::Nil, ::List, ::Continuation) = error("Cannot evaluate nil")
+
+# π: program
+# ɛ: environment
+# k: continuation
+function compute(π::Cons, ɛ::List, k::Continuation)
+    if car(π) == :
+    end
+end
+
+end
+=#
