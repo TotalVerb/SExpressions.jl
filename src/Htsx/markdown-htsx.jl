65a0b70cb89cac3ac66eb0ee200bfccb2039de8f
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-11-08 00:50:40 -0500

    Allow rendering LaTeX

--- src/Htsx/markdown-htsx.jl
+++ src/Htsx/markdown-htsx.jl
@@ -7,6 +7,7 @@ render(md::Base.Markdown.Paragraph)::List =
     List(:p, (render(x) for x in md.content)...)
 render(md::Base.Markdown.Link)::List =
     List(:a, List(List(:href, md.url)), (render(x) for x in md.text)...)
+render(md::Base.Markdown.LaTeX) = "\$$(md.formula)\$"
 render(md::String) = md
 
 end
