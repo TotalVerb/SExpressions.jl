629cafcccf72ec9a4452a83a4ec3d598a2342647
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-01-03 01:59:28 -0500

    delete useless bad line

--- src/Htsx/stdlib.jl
+++ src/Htsx/stdlib.jl
@@ -22,7 +22,6 @@ end
 joint(m::TagMatrix, t1, t2) = m.correlation[tuple(sort([t1, t2])...)]
 
 function relatedto(m::TagMatrix, t)
-    relevant = filter(x -> t in x[:tags], result)
     top = [(tag, joint(m, t, tag) / (m.popularity[tag] + 2))
            for tag in keys(m.popularity) if tag != t]
     filter!(x -> x[2] > 0, top)
