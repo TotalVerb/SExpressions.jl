a0c6160b4d4b8c5e4c8f6dbbb5f9281a06e31c6e
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-01-22 22:36:09 -0500

    Improve the tags library

--- src/Htsx/Tags.jl
+++ src/Htsx/Tags.jl
@@ -14,6 +14,11 @@ immutable TagMatrix
         DefaultDict{Tuple{String,String}, Int}(0))
 end
 
+"""
+    tags(m::TagMatrix)
+
+Return an iterable over all tags seen in the TagMatrix `m`.
+"""
 tags(m::TagMatrix) = keys(m.popularity)
 
 """
@@ -30,16 +35,32 @@ Return a `Vector` of tags, ordered from most popular to least.
 """
 popular(m::TagMatrix) = sort(collect(tags(m)), by=t -> -popularity(m, t))
 
+"""
+    joint(m::TagMatrix, t1, t2)
+
+Return the total weight of entries which have both `t1` and `t2` as tags.
+"""
 joint(m::TagMatrix, t1, t2) = m.correlation[tuple(sort([t1, t2])...)]
 
-function relatedto(m::TagMatrix, t)
+"""
+    relatedto(m::TagMatrix, tag, num=8)
+
+Return up to the top `num` tags related to `tag`.
+"""
+function relatedto(m::TagMatrix, t, num=8)
     top = [(tag, joint(m, t, tag) / (m.popularity[tag] + 2))
            for tag in keys(m.popularity) if tag != t]
     filter!(x -> x[2] > 0, top)
     sort!(top, by=x -> -x[2])
-    take(top, 5)
+    take(top, num)
 end
 
+"""
+    populate!(m::TagMatrix, tags, value=1)
+
+Add an entry to the tag matrix, whose tags are given by `tags`, and whose
+weight is given by `value`.
+"""
 function populate!(m::TagMatrix, tags, value)
     for tag in tags
         m.popularity[tag] += value
