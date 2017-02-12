154e9e391be43106a8889e28011f5179477a047c
Fengyang Wang <fengyang.wang.0@gmail.com>
2017-02-12 13:51:40 -0500

    Preliminary Documenter rendermd

--- src/Htsx/stdlib.jl
+++ src/Htsx/stdlib.jl
@@ -1,7 +1,15 @@
 module StdLib
 
+using SExpressions.Lists
 import ..MarkdownHtsx.render
+using Documenter.Writers.HTMLWriter: mdconvert
+using Documenter.Utilities.DOM: Node, TEXT
 
-rendermd(x) = render(Base.Markdown.parse(x))
+undomify(d::Node) = if d.name === TEXT
+    d.text
+else
+    list(d.name, map(undomify, d.nodes)...)
+end
+rendermd(x) = undomify(mdconvert(Base.Markdown.parse(x)))
 
 end
