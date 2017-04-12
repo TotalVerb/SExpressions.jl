f0ab3d7b5277979086a8e75d8a1fbdfbb40a7516
autodocs <autodocs>
2017-04-12 01:02:21 +0000

    build based on 6f1baa0

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,39 @@ var documenterSearchIndex = {"docs": [
     "page": "SExpressions.jl",
     "title": "SExpressions.jl",
     "category": "section",
-    "text": "Documentation for SExpressions.jl"
+    "text": "SExpressions.jl includes a roughly Racket-compatible parser for s-expressions."
+},
+
+{
+    "location": "index.html#SExpressions.Parser.parse",
+    "page": "SExpressions.jl",
+    "title": "SExpressions.Parser.parse",
+    "category": "Function",
+    "text": "Parse a Symbol or number or Keyword or Bool at the given index.\n\n\n\nParse a String at the given index.\n\n\n\nParse a list at the given index.\n\n\n\nParse an s-expression at the given index of the given AbstractString.\n\n\n\n"
+},
+
+{
+    "location": "index.html#SExpressions.Parser.parses",
+    "page": "SExpressions.jl",
+    "title": "SExpressions.Parser.parses",
+    "category": "Function",
+    "text": "Parse an entire string into a single list.\n\n\n\n"
+},
+
+{
+    "location": "index.html#SExpressions.Parser.parsefile",
+    "page": "SExpressions.jl",
+    "title": "SExpressions.Parser.parsefile",
+    "category": "Function",
+    "text": "Parse a file into a single list.\n\n\n\n"
+},
+
+{
+    "location": "index.html#Core-Parser-Interface-1",
+    "page": "SExpressions.jl",
+    "title": "Core Parser Interface",
+    "category": "section",
+    "text": "SExpressions.parse\nSExpressions.parses\nSExpressions.parsefile"
 },
 
 ]}
